e2d25d93ef8d435d4e418eaf8499c071a57923da
Allison Sun <allisunson@gmail.com>
2026-07-25 20:19:02 -0700

    logos

--- app/components/Sponsors.js
+++ app/components/Sponsors.js
@@ -11,14 +11,16 @@ const TIERS = [
   },
   {
     label: '贊助單位',
-    logos: ['/sponsors/china-airlines.svg', '/sponsors/taipei-music-center.svg', '/sponsors/chiayi-city.png', '/sponsors/chiayi-cultural-affairs.png', '/sponsors/chiayi-cultural-foundation.png', '/sponsors/coco-bubble-tea.png', '/sponsors/fapa.png', '/sponsors/tla-law.svg', '/sponsors/interline.png'],
-    heights: [60, 40, 48, 48, 60, 56, 72, 64, 40],
-    mobileHeights: [null, null, null, null, 48, null, null, null, null],
-    filters: [null, null, 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'grayscale(1) invert(1) contrast(20)', null, 'invert(1)'],
+    logos: ['/sponsors/china-airlines.svg', '/sponsors/taipei-music-center.svg', '/sponsors/chiayi-city.png', '/sponsors/chiayi-cultural-affairs.png', '/sponsors/chiayi-cultural-foundation.png', '/sponsors/coco-bubble-tea.png', '/sponsors/fapa.png', '/sponsors/tla-law.svg', '/sponsors/interline.png', '/sponsors/lin-tsung-ming.png', '/sponsors/cwcmf.png', '/sponsors/zhiqi-77.png'],
+    heights: [60, 40, 48, 48, 60, 56, 72, 64, 40, 72, 30, 34],
+    mobileHeights: [null, null, null, null, 48, null, null, null, null, 56, 24, 28],
+    filters: [null, null, 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'grayscale(1) invert(1) contrast(20)', null, 'invert(1)', null, null],
   },
   {
     label: '特別感謝',
-    logos: ['/sponsors/teresa.svg'],
+    logos: ['/sponsors/teresa.svg', '/sponsors/mumu-bath.png', '/sponsors/winson.png', '/sponsors/yumpling.png', '/sponsors/yunhai.png'],
+    heights: [40, 48, 96, 64, 52],
+    mobileHeights: [null, 38, 76, 52, 42],
   },
 ]
 
