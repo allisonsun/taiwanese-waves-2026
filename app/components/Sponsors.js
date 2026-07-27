31036bea5fceedb5625fd14327272f559158ca51
Allison Sun <allisunson@gmail.com>
2026-07-27 10:57:39 -0700

    logos

--- app/components/Sponsors.js
+++ app/components/Sponsors.js
@@ -10,17 +10,24 @@ const TIERS = [
     heights: [56, 64],
   },
   {
+    label: '合作單位',
+    logos: ['/sponsors/chiayi-cultural-affairs.png', '/sponsors/chiayi-city.png', '/sponsors/chiayi-cultural-foundation.png'],
+    heights: [46, 48, 60],
+    mobileHeights: [null, null, 48],
+    filters: ['brightness(0) invert(1)', 'brightness(0) invert(1)', 'brightness(0) invert(1)'],
+  },
+  {
     label: '贊助單位',
-    logos: ['/sponsors/china-airlines.svg', '/sponsors/taipei-music-center.svg', '/sponsors/chiayi-city.png', '/sponsors/chiayi-cultural-affairs.png', '/sponsors/chiayi-cultural-foundation.png', '/sponsors/coco-bubble-tea.png', '/sponsors/fapa.png', '/sponsors/tla-law.svg', '/sponsors/interline.png', '/sponsors/lin-tsung-ming.png', '/sponsors/cwcmf.png', '/sponsors/zhiqi-77.png'],
-    heights: [60, 40, 48, 48, 60, 56, 72, 64, 40, 72, 30, 34],
-    mobileHeights: [null, null, null, null, 48, null, null, null, null, 56, 24, 28],
-    filters: [null, null, 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'brightness(0) invert(1)', 'grayscale(1) invert(1) contrast(20)', null, 'invert(1)', null, null],
+    logos: ['/sponsors/china-airlines.svg', '/sponsors/taipei-music-center.svg', '/sponsors/coco-bubble-tea.png', '/sponsors/fapa.png', '/sponsors/tla-law.svg', '/sponsors/interline.png', '/sponsors/lin-tsung-ming.png', '/sponsors/cwcmf.png', '/sponsors/zhiqi-77.png', '/sponsors/colega-ai.png', '/sponsors/nina.png'],
+    heights: [60, 40, 56, 72, 64, 40, 72, 30, 34, 40, 48],
+    mobileHeights: [null, null, null, null, null, null, 56, 24, 28, null, 40],
+    filters: [null, null, 'brightness(0) invert(1)', 'grayscale(1) invert(1) contrast(20)', null, 'invert(1)', null, null, null, null, null],
   },
   {
     label: '特別感謝',
-    logos: ['/sponsors/teresa.svg', '/sponsors/mumu-bath.png', '/sponsors/winson.png', '/sponsors/yumpling.png', '/sponsors/yunhai.png'],
-    heights: [40, 48, 96, 64, 52],
-    mobileHeights: [null, 38, 76, 52, 42],
+    logos: ['/sponsors/teresa.svg', '/sponsors/mumu-bath.png', '/sponsors/winson.png', '/sponsors/yumpling.png', '/sponsors/yunhai.png', '/sponsors/taiwan-bear-house.png'],
+    heights: [40, 48, 96, 64, 52, 80],
+    mobileHeights: [null, 38, 76, 52, 42, 64],
   },
 ]
 
